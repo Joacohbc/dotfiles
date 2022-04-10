3a64b20a48501c3bafe76be2827d2d83a76ffef3
Joacohbc <69821825+Joacohbc@users.noreply.github.com>
2022-04-10 16:55:07 -0300

    Fix in script update.sh
    - If the PC don't have internet, prints "None"

--- Scripts/scriptsPolybar/update.sh
+++ Scripts/scriptsPolybar/update.sh
@@ -1,6 +1,6 @@
-#INFO: MUESTRA LA CANTIDAD ACTUALIZACIONES (PACMAN y AUR)
+#INFO: MUESTRA LA CANTIDAD ACTUALIZACIONES USANDO YAY (PACMAN y AUR) 
 
-NUM_UPDATE=$(yay -Qu | wc -l)
+NUM_UPDATE=$(yay -Qqu 2>> /dev/null | wc -l)
 if [[ $NUM_UPDATE -eq 0 ]]; then 
     echo "None"
 else 
